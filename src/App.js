d48d65f8d7fc3ca4364e49e8ab429572a22a6467
Bawa Abba <abbabawa6@gmail.com>
2021-07-24 14:52:14 +0100

    Generic form page created and worked on

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Header from './components/Header';
 import SideBar from './components/SideBar';
 import SiteFooter from './components/Footer';
 import Dashboard from './pages/Dashboard';
+import SiteForm from './pages/Form';
 
 function App() {
 	const [show, setShow] = useState(false);
 
   	const handleClose = () => setShow(false);
   	const handleShow = () => setShow(true);
+
+	const handleSubmit = (data)=>{
+		console.log(data)
+        alert("Form submitted")
+    }
   return (
 	<Container fluid>
 		<Row>
@@ -37,6 +43,9 @@ function App() {
 				</div>
 				<Row className="py-3">
 					<Switch>
+						<Route path="/form">
+							<SiteForm handleSubmit={handleSubmit} />
+						</Route>
 						<Route path="/">
 							<Dashboard />
 						</Route>
